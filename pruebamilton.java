34b29fdf62762f258120ea071ecae0fe1647273b
milton <milton.desing@gmail.com>
2022-08-23 13:05:57 -0500

    Initial commit

---
+++ pruebamilton.java
@@ -0,0 +1,8 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.net.SocketTimeoutException;
+
+public class pruebamilton {
+    public int numero=3;
+  //ESTO ES UNA PRUEBA DE CONFIGURACIÓN CON INTELLIJ
+}
